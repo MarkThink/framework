70c5dbc64dbd88043ffec358c7c0fdf386f37c89
markthink <dxwsker@qq.com>
2015-09-23 00:14:27 +0800

    控制器重构

--- src/app.php
+++ src/app.php
@@ -2,32 +2,12 @@
 
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
-use Symfony\Component\HttpFoundation\Response;
-
-class LeapYearController
-{
-    public function indexAction($year)
-    {
-        if(is_leap_year($year)){
-            return new Response('Yep, this is a leap year!');
-        }
-        return new Response('Nope, this is not a leap year.');
-    }
-}
-
-function is_leap_year($year=null)
-{
-    if(null === $year){
-        $year = data('Y');
-    }
-    return 0==$year % 400 || ( 0 == $year % 4 && 0 != $year %100);
-}
 
 $routes = new RouteCollection();
 
 $routes->add('leap_year', new Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => 'LeapYearController::indexAction',
+    '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
 )));
 
 
