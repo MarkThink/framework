faf5499da527ce0fc537fddd37e870cf541a7762
markthink <dxwsker@qq.com>
2015-09-22 10:42:26 +0800

    URL路由地址更新

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,8 +1,2 @@
-<?php
-
-//$input = $request->get('name', 'World');
-//$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
-
-$name = $request->get('name','World'); ?>
 
 Hello <?php echo htmlspecialchars($name,ENT_QUOTES,'utf-8');?>
