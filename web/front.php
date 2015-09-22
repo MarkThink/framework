3f7857fc71f42b93010832bf9b8cb187161d177e
markthink <dxwsker@qq.com>
2015-09-22 11:58:25 +0800

    增加自定义渲染函数

--- web/front.php
+++ web/front.php
@@ -32,15 +32,22 @@ $matcher = new UrlMatcher($routes,$context);
 //$dumper = new PhpMatcherDumper($routes);
 //$dump_routes = $dumper->dump();
 
-try{
-	extract($matcher->match($request->getPathInfo()),EXTR_SKIP);
+function render_template($request)
+{
+	extract($request->attributes->all(),EXTR_SKIP);
 	ob_start();
 	include sprintf(__DIR__.'/../src/pages/%s.php',$_route);
-	$response = new Response(ob_get_clean());
+	return new Response(ob_get_clean());
+}
+
+try{
+	$request->attributes->add($matcher->match($request->getPathInfo()));
+	$response = call_user_func('render_template',$request);
 }catch (ResourceNotFoundException $e){
 	$response = new Response('Not Found',404);
 }catch(\Exception $e){
 	$response = new Response('An error occurred',500);
 }
+
 
 $response->send();
