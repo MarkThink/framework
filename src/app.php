c8bbe4d2fb28b0dc08b7d35b65514b0c1b91736f
markthink <dxwsker@qq.com>
2015-09-22 18:11:32 +0800

    增加自定义函数-闰年判断

--- src/app.php
+++ src/app.php
@@ -2,10 +2,17 @@
 
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+function is_leap_year($year=null)
+{
+    if(null === $year){
+        $year = data('Y');
+    }
+    return 0==$year % 400 || ( 0 == $year % 4 && 0 != $year %100);
+}
 
 $routes = new RouteCollection();
-
-
 
 $routes->add('hello',new Route('/hello/{name}',array(
     'name'=>'world',
@@ -16,6 +23,14 @@ $routes->add('hello',new Route('/hello/{name}',array(
         return $response;
     }
 )));
-$routes->add('bye',new Route('/bye'));
+$routes->add('leap_year',new Route('/is_leap_year/{year}',array(
+    'year' => null,
+    '_controller'=>function($request){
+        if(is_leap_year($request->attributes->get('year'))){
+            return new Response('Yep, this is a leap year!');
+        }
+        return new Response('Nope, this is not a leap year.');
+    }
+)));
 
 return $routes;
