7b0144f68a58f0bfa2f5a85dba8df260789561b9
markthink <dxwsker@qq.com>
2015-09-23 22:05:55 +0800

    增加事件调度组件-第三个参数值从大到小优先执行 $dispatcher->addListener('response', function (){},1); $dispatcher->addListener('response', function (){},0);

--- web/front.php
+++ web/front.php
@@ -9,7 +9,32 @@ use Symfony\Component\Routing\Matcher\UrlMatcher;
 
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
 use Simplex\Framework;
+
+$dispatcher = new EventDispatcher();
+$dispatcher->addListener('response',function(Simplex\ResponseEvent $event){
+    $response = $event->getResponse();
+
+    if($response->isRedirection()
+        || ($response->headers->has('Content-Type') && false === strpos($response->headers->get('Content-Type'), 'html'))
+        || 'html' !== $event->getRequest()->getRequestFormat()
+    ){
+        return;
+    }
+
+    $response->setContent($response->getContent().'GA CODE');
+},0);
+
+$dispatcher->addListener('response', function (Simplex\ResponseEvent $event) {
+    $response = $event->getResponse();
+    $headers = $response->headers;
+
+    if (!$headers->has('Content-Length') && !$headers->has('Transfer-Encoding')) {
+        $headers->set('Content-Length', strlen($response->getContent()));
+    }
+},1);
 
 $request = Request::createFromGlobals();
 
@@ -19,7 +44,7 @@ $context = new RequestContext();
 $matcher = new UrlMatcher($routes,$context);
 $resolver = new ControllerResolver();
 
-$framework = new Framework($matcher,$resolver);
+$framework = new Framework($dispatcher,$matcher,$resolver);
 $response = $framework->handle($request);
 
 $response->send();
