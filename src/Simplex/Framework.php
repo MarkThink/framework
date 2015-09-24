7f251048dcb61d1faa8e87a5c27c009ca4097acd
markthink <dxwsker@qq.com>
2015-09-24 22:43:30 +0800

    微框架完结

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -3,12 +3,6 @@
 namespace Simplex;
 
 use Symfony\Component\HttpKernel\HttpKernel;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
-use Symfony\Component\HttpKernel\EventListener\ResponseListener;
 
 class Framework extends HttpKernel
 {
--- web/front.php
+++ web/front.php
@@ -7,7 +7,11 @@ use Symfony\Component\HttpFoundation\Request;
 $request = Request::createFromGlobals();
 
 $routes = include __DIR__.'/../src/app.php';
+
 $sc = include __DIR__.'/../src/container.php';
+$sc->setParameter('routes',$routes);
+$sc->setParameter('charset','utf-8');
+$sc->setParameter('debug',true);
 
 $response = $sc->get('framework')->handle($request);
 
